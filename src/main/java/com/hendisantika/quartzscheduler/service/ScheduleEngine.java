e02809de7767528f6fab62964497ef4020f7d9c4
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 10:06:44 +0700

    Add start in ScheduleEngine 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/quartzscheduler/service/ScheduleEngine.java
+++ src/main/java/com/hendisantika/quartzscheduler/service/ScheduleEngine.java
@@ -1,9 +1,17 @@
 package com.hendisantika.quartzscheduler.service;
 
+import com.hendisantika.quartzscheduler.model.Schedule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +32,20 @@ public class ScheduleEngine {
     public static final String PROCESS_ID_KEY = "JOB_PROCESS_ID";
 
     private final Scheduler scheduler;
+
+    public void start(Schedule schedule) {
+        Trigger logTrigger = createTrigger(schedule);
+        JobDetail jobDetail = createJobDetail(schedule);
+        try {
+            scheduler.scheduleJobs(
+                    Stream.of(
+                                    new SimpleImmutableEntry<>(jobDetail, Collections.singleton(logTrigger))
+                            )
+                            .collect(Collectors.toMap(SimpleImmutableEntry::getKey, SimpleImmutableEntry::getValue)),
+                    true
+            );
+        } catch (SchedulerException e) {
+            log.error("Unable to schedule process {} for monitoring", schedule.getId());
+        }
+    }
 }
