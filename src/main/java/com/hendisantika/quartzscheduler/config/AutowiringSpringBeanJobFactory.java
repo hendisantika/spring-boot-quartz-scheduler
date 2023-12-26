b674437e88d8b484c70553dedd14568754b294b7
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 10:01:15 +0700

    Add createJobInstance in AutowiringSpringBeanJobFactory 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/quartzscheduler/config/AutowiringSpringBeanJobFactory.java
+++ src/main/java/com/hendisantika/quartzscheduler/config/AutowiringSpringBeanJobFactory.java
@@ -1,5 +1,6 @@
 package com.hendisantika.quartzscheduler.config;
 
+import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -22,5 +23,12 @@ public final class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory i
     @Override
     public void setApplicationContext(final ApplicationContext context) {
         this.beanFactory = context.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+        final Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
     }
 }
