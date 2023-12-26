e2b81dde27ac4b12fe6d415aec909630ab5c170a
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 10:01:05 +0700

    Add setApplicationContext in AutowiringSpringBeanJobFactory 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/quartzscheduler/config/AutowiringSpringBeanJobFactory.java
+++ src/main/java/com/hendisantika/quartzscheduler/config/AutowiringSpringBeanJobFactory.java
@@ -1,6 +1,7 @@
 package com.hendisantika.quartzscheduler.config;
 
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
@@ -17,4 +18,9 @@ import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 public final class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
 
     private AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext context) {
+        this.beanFactory = context.getAutowireCapableBeanFactory();
+    }
 }
