c5989d531a6f22a479c677d664f70b52fc6802bf
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 10:02:16 +0700

    Add schedulerFactoryBean in QuartzConfiguration 🫸🌀✏️📗 :octocat:🐧🐳

--- src/main/java/com/hendisantika/quartzscheduler/config/QuartzConfiguration.java
+++ src/main/java/com/hendisantika/quartzscheduler/config/QuartzConfiguration.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +35,17 @@ public class QuartzConfiguration {
         AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
         jobFactory.setApplicationContext(context);
         return jobFactory;
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(ApplicationContext context, DataSource dataSource) {
+        Properties properties = new Properties();
+        properties.putAll(quartzProperties.getProperties());
+
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setQuartzProperties(properties);
+        schedulerFactoryBean.setJobFactory(springBeanJobFactory(context));
+        schedulerFactoryBean.setDataSource(dataSource);
+        return schedulerFactoryBean;
     }
 }
