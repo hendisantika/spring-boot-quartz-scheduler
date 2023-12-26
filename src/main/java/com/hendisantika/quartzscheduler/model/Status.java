5c2a46c7a49372ce747ad82cb39b41e3d61e961a
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 10:04:43 +0700

    Add Status Enum 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzscheduler/model/Status.java
@@ -0,0 +1,17 @@
+package com.hendisantika.quartzscheduler.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-quartz-scheduler
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/26/23
+ * Time: 10:04
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Status {
+    UNKNOWN,
+    ENABLED,
+    DISABLED
+}
