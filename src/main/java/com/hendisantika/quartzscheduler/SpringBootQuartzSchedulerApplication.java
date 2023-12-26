359b32cb8ef44f039ce865a175a3426dcbacfb8a
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 09:54:13 +0700

    Create starter project 🫸🌀✏️📗 :octocat:🐧🐳⬆

---
+++ src/main/java/com/hendisantika/quartzscheduler/SpringBootQuartzSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.quartzscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuartzSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuartzSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/quartzscheduler/SpringBootQuartzSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.quartzscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuartzSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
