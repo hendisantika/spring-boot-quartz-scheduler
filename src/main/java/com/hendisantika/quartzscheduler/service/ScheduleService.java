f878b55a2eb0feba4b709eff87d7425c1aaf09d1
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 10:03:30 +0700

    Add ScheduleService 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzscheduler/service/ScheduleService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.quartzscheduler.service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-quartz-scheduler
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/26/23
+ * Time: 10:03
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ScheduleService {
+    Schedule create(Schedule schedule);
+
+    Schedule getSchedule(UUID id);
+
+    Schedule stopSchedule(UUID id);
+
+    Schedule startSchedule(UUID id);
+
+    List<Schedule> getSchedules();
+}
