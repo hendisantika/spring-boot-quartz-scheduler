d782da57e0169a2ee7fe72e868c0de8ba9b61fbf
hendisantika <hendisantika@yahoo.co.id>
2023-12-26 10:00:51 +0700

    Add AutowiringSpringBeanJobFactory 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/quartzscheduler/config/AutowiringSpringBeanJobFactory.java
@@ -0,0 +1,20 @@
+package com.hendisantika.quartzscheduler.config;
+
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-quartz-scheduler
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/26/23
+ * Time: 10:00
+ * To change this template use File | Settings | File Templates.
+ */
+public final class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    private AutowireCapableBeanFactory beanFactory;
+}
